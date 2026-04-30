24eb553d9a7b10e8bee387bb6b7aa8c32cd248b0
k-cicek <kcicek1@ogr.iu.edu.tr>
2026-05-01 00:07:05 +0300

    update project configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Allow local network host for Next dev HMR access
   allowedDevOrigins: ["192.168.1.105"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
